4341d34a6e8b02b865016c44b40a8f511b963f6e
wangxiao <1988wangxiao@gmail.com>
2012-10-31 00:33:14 +0800

    timeLine

---
+++ timeLine.js
@@ -0,0 +1,407 @@
+//test 用于单元测试
+var test = test || {};
+test.data = [
+//2011.8.17
+	{
+		id:'132131fesef',
+		time:'1345168843044',
+		width:329,
+		height:398,
+		imageURL:"http://www.yule8gua.com/uploads/allimg/091226/1_091226163059_4.jpg"
+	},
+	{
+		id:'132131fese23f',
+		time:'1345168843044',
+		width:600,
+		height:835,
+		imageURL:"http://www.365zn.com/mrl/pic/mrpic2006in477.jpg"
+	},
+	{
+		id:'132131fesef',
+		time:'1345168843044',
+		width:329,
+		height:398,
+		imageURL:"http://www.yule8gua.com/uploads/allimg/091226/1_091226163059_4.jpg"
+	},
+	{
+		id:'132131fesef',
+		time:'1345168843044',
+		width:600,
+		height:835,
+		imageURL:"http://www.365zn.com/mrl/pic/mrpic2006in477.jpg"
+	},
+	{
+		id:'132131fesef',
+		time:'1345168843044',
+		width:500,
+		height:441,
+		imageURL:"http://pic.66wz.com/0/01/03/57/1035773_781886.jpg"
+	},
+	{
+		id:'132131fesef',
+		time:'1345168843044',
+		width:329,
+		height:398,
+		imageURL:"http://www.yule8gua.com/uploads/allimg/091226/1_091226163059_4.jpg"
+	},
+	{
+		id:'132131fesef',
+		time:'1345168843044',
+		width:600,
+		height:835,
+		imageURL:"http://www.365zn.com/mrl/pic/mrpic2006in477.jpg"
+	},
+	{
+		id:'132131f12esef',
+		time:'1345168843044',
+		width:600,
+		height:407,
+		imageURL:"http://pic.people.com.cn/mediafile/201003/11/F201003111441522664926587.jpg"
+	},
+	{
+		id:'1a55sldflwe',
+		time:'1345131939576',
+		width:600,
+		height:407,
+		imageURL:"http://pic.people.com.cn/mediafile/201003/11/F201003111441522664926587.jpg"
+	},
+//2011.8.16	
+	{
+		id:'1321wf2weesef',
+		time:'1345130185274',
+		width:324,
+		height:460,
+		imageURL:"http://www.sc.xinhuanet.com/content/2006-04/12/xin_4504031216212332931194.jpg"
+	},
+	{
+		id:'1321wf3weesef',
+		time:'1345130185274',
+		width:600,
+		height:835,
+		imageURL:"http://www.365zn.com/mrl/pic/mrpic2006in477.jpg"
+	},
+	{
+		id:'1321wfw113eesef',
+		time:'1345130185274',
+		width:600,
+		height:407,
+		imageURL:"http://pic.people.com.cn/mediafile/201003/11/F201003111441522664926587.jpg"
+	},
+	{
+		id:'1321wfw213eesef',
+		time:'1345130185274',
+		width:600,
+		height:835,
+		imageURL:"http://www.365zn.com/mrl/pic/mrpic2006in477.jpg"
+	},
+	{
+		id:'1321wf2weesef',
+		time:'1345130185274',
+		width:324,
+		height:460,
+		imageURL:"http://www.sc.xinhuanet.com/content/2006-04/12/xin_4504031216212332931194.jpg"
+	},
+	{
+		id:'1321wf3weesef',
+		time:'1345130185274',
+		width:600,
+		height:835,
+		imageURL:"http://www.365zn.com/mrl/pic/mrpic2006in477.jpg"
+	},
+	{
+		id:'1321wfw113eesef',
+		time:'1345130185274',
+		width:600,
+		height:407,
+		imageURL:"http://pic.people.com.cn/mediafile/201003/11/F201003111441522664926587.jpg"
+	},
+	{
+		id:'1321wfw213eesef',
+		time:'1345130185274',
+		width:600,
+		height:835,
+		imageURL:"http://www.365zn.com/mrl/pic/mrpic2006in477.jpg"
+	},
+	{
+		id:'1321wf2weesef',
+		time:'1345130185274',
+		width:324,
+		height:460,
+		imageURL:"http://www.sc.xinhuanet.com/content/2006-04/12/xin_4504031216212332931194.jpg"
+	},
+	{
+		id:'1321wf3weesef',
+		time:'1345130185274',
+		width:600,
+		height:835,
+		imageURL:"http://www.365zn.com/mrl/pic/mrpic2006in477.jpg"
+	},
+	{
+		id:'1321wfw113eesef',
+		time:'1345130185274',
+		width:600,
+		height:407,
+		imageURL:"http://pic.people.com.cn/mediafile/201003/11/F201003111441522664926587.jpg"
+	},
+	{
+		id:'1321wfw213eesef',
+		time:'1345130185274',
+		width:600,
+		height:835,
+		imageURL:"http://www.365zn.com/mrl/pic/mrpic2006in477.jpg"
+	},
+//2011.8.15	
+	{
+		id:'1321wfw123eesef',
+		time:'1347667200000',
+		width:600,
+		height:407,
+		imageURL:"http://pic.people.com.cn/mediafile/201003/11/F201003111441522664926587.jpg"
+	},
+	{
+		id:'1321wfw123eesef',
+		time:'1347667200000',
+		width:329,
+		height:398,
+		imageURL:"http://www.yule8gua.com/uploads/allimg/091226/1_091226163059_4.jpg"
+	},
+	{
+		id:'1321wfw123eesef',
+		time:'1347667200000',
+		width:600,
+		height:407,
+		imageURL:"http://pic.people.com.cn/mediafile/201003/11/F201003111441522664926587.jpg"
+	},
+	{
+		id:'1321wfw123eesef',
+		time:'1347667200000',
+		width:329,
+		height:398,
+		imageURL:"http://www.yule8gua.com/uploads/allimg/091226/1_091226163059_4.jpg"
+	},
+	{
+		id:'1321wfw123eesef',
+		time:'1347667200000',
+		width:329,
+		height:398,
+		imageURL:"http://www.yule8gua.com/uploads/allimg/091226/1_091226163059_4.jpg"
+	},
+	{
+		id:'1321wfw123eesef',
+		time:'1347667200000',
+		width:600,
+		height:407,
+		imageURL:"http://pic.people.com.cn/mediafile/201003/11/F201003111441522664926587.jpg"
+	}
+];
+/** 豆子公司面试题答案
+ *  timeLine 组件
+ *  @author 尼奥 
+ *  @email 1988wangxiao@gmail.com
+ *  @github  http://wangxiao.github.com
+*/
+
+//import jquery.sizzle();
+//import jquery.html();
+
+//组件部分
+//@param {String} element 容器id
+//@param {String} options.url 数据源
+//@param {Object} Options.data 直接传入数据
+//@param {Number} options.maxWidth 图片展示最大宽
+//@param {Number} options.maxHeight 图片展示最大高
+
+function TimeLine(element,options){
+	 
+	this.element = jQuery('#'+element);
+	this.options = options || {};
+	this.url = options.url || {};
+	this.data = options.data || {};
+
+	//每张图片最大的宽和高
+	this.maxWidth = options.maxWidth || 160;
+	this.maxHeight = options.maxHeight || 160;
+
+	//下张图片的地址
+	this.nextURL = '';
+
+	//标志此时是否在加载数据
+	this.flag = false;
+
+	this.c().main();
+};
+
+//数据相关
+TimeLine.prototype.m = function(){
+	var me = this;
+	return {
+
+		//将数据按照日期分割
+		getDataByDate : function(data){
+			var newData = {};
+			var oldTime;
+			var flag = 0;
+			for(var i=0;i<data.length;i++){
+				var time = me.tools.formatDate(data[i].time);
+				if(flag){
+					if(oldTime == time){
+						newData[time].push(data[i]);
+					}else{
+						newData[time] = [];
+						newData[time].push(data[i]);
+						oldTime = time;
+					}; 
+				}else{
+					flag = 1;
+					oldTime = time;
+					newData[time] = [];
+					newData[time].push(data[i]);
+				}
+			};
+			return newData;
+		},
+
+		//将图片控制在160x160px，data是一张图片的数据
+		setImage : function(data){
+			if(data.width > data.height){
+				data.minWidth = me.maxWidth;
+				data.minHeight = (data.height/data.width)*me.maxWidth;
+			}else{
+				data.minHeight = me.maxHeight;
+				data.minWidth = (data.width/data.height)*me.maxHeight;
+			}
+			return data;
+		},
+
+		//处理后的照片数据
+		getData : function(data){
+			for(var key in data){
+				me.m().setImage(data[key]);
+			}
+			return me.m().getDataByDate(data);
+		}
+
+	};
+};
+
+//视图相关
+TimeLine.prototype.v = function(){
+	var me = this; 
+	
+	//前端模板，整个界面
+	me.tpl = [
+		"<%for(var key in data){%>",
+			"<!-- 一天的模板 -->",
+			"<div>",
+			"<h2><%=key%></h2>",
+			"<%for(var i = 0;i<data[key].length;i++){%>",
+				"<img src=<%=data[key][i].imageURL%> style=width:<%=data[key][i].minWidth%>px;height:<%=data[key][i].minHeight%>px;>",
+				"<!-- 五张图片一换行 -->",
+				"<%if((i+1)%5 == 0){%>",
+					"<br>",
+				"<%}%>",
+			"<%}%>",
+			"</div>",
+			"<!-- 一天的结束 -->",
+		"<%}%>"
+	].join('');
+
+	return {
+
+		//设置模板
+		setTpl : function(tpl){
+			me.tpl = String(tpl);
+		},
+
+		//获取模板
+		getTpl : function(){
+			return me.tpl;
+		}
+
+	}
+};
+
+//控制器
+TimeLine.prototype.c = function(){
+	var me = this; 
+	return {
+
+		//主入口
+		main : function(){
+
+			//异步取数据
+			function getData(url){
+				url = url + '&jsoncallback=?';
+				me.flag = true;
+				jQuery.getJSON(url,function(data){
+					me.nextURL = data.nextURL;
+					//me.c().show(data);
+					me.c().show(test.data);
+					me.flag = false;
+				});
+			};
+			getData(me.url);
+			me.tools.scrollBottom();
+			//当滚到底部，获取数据
+			$(document).on('scrollBottom',function(){
+				if(!me.flag){
+					//getData(me.nextURL);
+					getData(me.url);
+				}
+			});
+		},
+
+		//传入原始的data形式，并显示图片
+		show : function(data){
+
+			var tpl = me.v().getTpl();
+			var tplFun = baidu.template(tpl);
+			var temp = {};
+			temp.data = me.m().getData(data);
+			var htmlTpl = tplFun(temp);
+			me.element.append(htmlTpl);
+		}
+
+
+	};
+
+};
+
+//工具方法
+TimeLine.prototype.tools = {
+
+	//将时间戳输出为日期
+	//source输入时间戳，pattern是返回格式，如“y-m-d”返回"2012-08-16"
+	formatDate : function(source,pattern){
+		var date = new Date(Number(source));
+
+		//年
+		var y = date.getFullYear();
+		
+		//月
+		var m = date.getMonth()+1;
+		m = m>9?m:'0'+m;
+
+		//日
+		var d = date.getDate();
+		d = d>9?d:'0'+d;
+
+		var pattern = pattern||'y-m-d';
+		return pattern.replace(/y/,y)
+				.replace(/m/,m)
+				.replace(/d/,d);
+	},
+
+	//滚到屏幕下方触发，派发全局scrollBottom事件
+	scrollBottom : function() {
+		jQuery(window).scroll(function(){
+			var scrollTop = jQuery(document).scrollTop();
+			var winHeight = jQuery(window).height();
+			var docHeight = jQuery(document).height();
+			if(scrollTop >= docHeight - winHeight - 200){
+				jQuery(document).trigger('scrollBottom');
+			}
+		});
+	}
+
+};
